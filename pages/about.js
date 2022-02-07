41b06eec6299901c6c33de7659c8e0f65b571bb1
minshxxx <minshxxx@gmail.com>
2022-02-07 23:37:00 +0900

    1 6 Custom App

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,8 @@
-import NavBar from "./components/NavBar";
+import NavBar from "../components/NavBar";
 
 export default function about() {
   return (
     <div>
-      <NavBar />
       <h1>about us</h1>
     </div>
   );
